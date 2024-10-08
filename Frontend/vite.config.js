e5f85ff3895f197d6800da0fe8c5aaa54a8c90e9
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-08 13:31:47 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:7000', // Backend server
+        target: 'https://ministore-backend.onrender.com', // Backend server
         changeOrigin: true,
       },
     },
